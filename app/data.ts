9b8413935872a2ea650f65555fcda5e901f61aa6
caseyanderson <cta@caseyanderson.com>
2025-02-26 16:31:17 -0800

    laksjdf

--- app/data.ts
+++ app/data.ts
@@ -137,19 +137,18 @@ export const BLOG_POSTS: BlogPost[] = [
         link: '/blog/rainforest',
         uid: 'blog-10',
     },
-    {
+    /**{
         title: 'Changes: 64 Studies for 6 Harps, James Tenney',
         description: 'recovered and completed this massive, unfinished Tenney project for archiving, recording, and performance',
         link: '/blog/changes',
         uid: 'blog-11',
-    },
-    /**{
+    },**/
+    {
         title: 'The Experimental Music Yearbook',
         description: 'a repository and periodic festival archiving the methods and styles of artists working in the experimental music tradition',
         link: '/blog/emy',
         uid: 'blog-12',
     },
-**/
     ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
